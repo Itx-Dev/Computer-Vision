7047974e9c51de1debffad0380e70bedc2bf2486
Devin Reichenbach <dr2676@engr.ship.edu>
2024-02-06 11:17:25 -0500

    Initial Commit

---
+++ Assignment_2_ENGR391/Problem1.py
@@ -0,0 +1,51 @@
+from email.mime import image
+
+import numpy as np
+# Define file name for Image Matrix
+inputMatrixFile = "Image Matrix.txt"
+inputList = list()
+
+# Define convolution mask
+mask = np.array([-1, 0, 1])
+
+# Open file to read-only
+with open(inputMatrixFile, 'r') as f:
+    # Iterate over each line and converting strings to ints
+    for line in f:
+        intList = [int(i) for i in line.split()]
+        inputList.append(intList)   # Add list to another list for 2D array
+
+matrix = np.array(inputList)
+# Pad matrix with zeros
+matrix = np.pad(matrix, 1, mode='constant', constant_values=0)
+# Get rid of top and bottom rows being padded
+matrix = np.array(matrix[1:-1])
+
+
+def convolution(image, givenMask):
+    answerArray = np.empty([0,5])
+    givenMask = np.flip(givenMask)
+
+    for i in range(3,5):
+        #print(image[:i])
+        #print(givenMask[-i:])
+        answer = np.sum(np.matmul(image[:i], givenMask[-i:]))
+        answerArray = np.append(answerArray, answer)
+
+    for i in range(1,3):
+        answer = np.sum(np.matmul(image[i:], givenMask[:-i]))
+        answerArray = np.append(answerArray, answer)
+
+    return answerArray
+
+# Split Matrix into 4 separate ones
+for i in range(0, 10):
+    newMat = np.array_split(matrix[i], 4)
+    # Limit Matrix to have min value of 0 and max value of 255
+    np.append(newMat, newMat)
+
+    for mat in range(0,4):
+        answer = convolution(newMat[mat], mask)
+        clippedMatrix = np.clip(answer, 0, 255)
+        print(clippedMatrix)
+
