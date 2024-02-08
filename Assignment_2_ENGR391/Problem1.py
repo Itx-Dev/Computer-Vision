a47e103de5f637b451f7a890bd5220709763b2af
Devin Reichenbach <dr2676@engr.ship.edu>
2024-02-07 19:02:35 -0500

    Finished Convolution program

--- Assignment_2_ENGR391/Problem1.py
+++ Assignment_2_ENGR391/Problem1.py
@@ -1,6 +1,23 @@
 import numpy as np
+
+def convolution(image, mask):
+    # Define size of rows and cols
+    rows, cols = matrix.shape
+    size = mask.shape[0]
+    # Set up array for storage of answers
+    answer = np.zeros((rows, cols - size + 1))
+
+    # Loop over to perform convolution operation
+    for i in range(rows):
+        for j in range(cols - size + 1):
+            answer[i, j] = np.sum(matrix[i, j:j+size] * mask)
+
+    # Return answer
+    return answer
+
 # Define file name for Image Matrix
 inputMatrixFile = "Image Matrix.txt"
+# Create empty list to store .txt file content
 inputList = list()
 
 # Define convolution mask
@@ -13,45 +30,18 @@ with open(inputMatrixFile, 'r') as f:
         intList = [int(i) for i in line.split()]
         inputList.append(intList)   # Add list to another list for 2D array
 
-matrix = np.array(inputList)
+# Create a numpy array from string list, with data type integer
+matrix = np.array(inputList, dtype=int)
 # Pad matrix with zeros
 matrix = np.pad(matrix, 1, mode='constant', constant_values=0)
 # Get rid of top and bottom rows being padded
 matrix = np.array(matrix[1:-1])
 
-def convolution(image, givenMask):
-    answerArray = np.empty([0,5])
-    givenMask = np.flip(givenMask)
+# Call convolution function with original image and mask
+answer = convolution(matrix, mask)
+# Clip the convolution matrix so all negative values are 0 and values over 255 are 255
+clippedAnswer = np.clip(answer, 0, 255)
 
-    #print(np.convolve(image, givenMask, mode="same"))
-
-    for i in range(2,4):
-        answer = np.sum(np.matmul(image[:i], givenMask[-i:]))
-        answerArray = np.append(answerArray, answer)
-
-    for i in range(1,2):
-        answer = np.sum(np.matmul(image[i:], givenMask[:-i]))
-        answerArray = np.append(answerArray, answer)
-
-    return answerArray
-
-# Split Matrix into 4 separate ones
-for i in range(0, 10):
-    newMat = np.array_split(matrix[i], 4)
-    # Store each 1x3 array into 4 variables
-    col1 = convolution(newMat[0], mask)
-    col2 = convolution(newMat[1], mask)
-    col3 = convolution(newMat[2], mask)
-    col4 = convolution(newMat[3], mask)
-
-    # Combine aray into single array
-    returnArray = np.concatenate([col1, col2, col3, col4])
-    # Limit Matrix to have min value of 0 and max value of 255
-    returnArray = np.clip(returnArray, 0, 255)
-    print(returnArray)
-
-
-
-
+print(clippedAnswer)
 
 
