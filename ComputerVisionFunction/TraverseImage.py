003578ad33bbbf8d43302038b5cdae29d0068b71
Devin <dr2676@ship.edu>
2024-02-09 17:55:43 -0500

    Fixed Convolution Syntax Errors

---
+++ ComputerVisionFunction/TraverseImage.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def traverseImage(image):
+    # Create a 3x3 mask
+    mask = np.ones((3, 3))
+
+    # Define height and width of image
+    height, width = image.shape
+    # Loop over entire image
+    for i in range(1, height - 1):
+        for j in range(1, width - 1):
+            # Extract 3x3 neighborhood of image
+            neighborhoodMatrix = image[i - 1: i + 2, j - 1: j + 2]
